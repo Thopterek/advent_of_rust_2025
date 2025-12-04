b1a1c0658890965e55ed19b4f5d12da89b747abc
Thopterek <sumplous.sumplous@gmail.com>
2025-12-04 19:51:21 +0100

    back to the first day slowly figuring out Rust methods

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -8,12 +8,27 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+// left from 0 -> one click 99
+// right from 99 -> one click 0
 fn main() {
     println!("Day 1 start!");
+    let mut dial: i32 = 50;
+    let mut count: i32 = 0;
     if let Ok(lines) = read_lines("../input/rotation.txt") {
         for line in lines.map_while(Result::ok) {
-            println!("{}", line);
+            dial = move_dial(dial, line);
+            if dial == 0 {
+                count += 1;
+            }
         }
+    }
+    else {
+        println!("Where is the damn file");
+    }
+}
+
+fn move_dial(current_dial: i32, next_move: str) -> i32 {
+    if next_move.starts_with('R') {
     }
 }
 
