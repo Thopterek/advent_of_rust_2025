bfc9a714d737d06662934628d232f1b4adb85349
Thopterek <sumplous.sumplous@gmail.com>
2025-12-05 15:28:06 +0100

    setup for day2, loading the input

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Day 2, lets goooo");
+    if let Ok(lines) = read_lines("../input/puzzle.txt") {
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line);
+        }
+    }
+    else {
+        println!("Where is that file?");
+    }
 }
