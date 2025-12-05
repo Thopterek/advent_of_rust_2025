1970ea0b853b4a31fc133cc501bc798c747f3009
Thopterek <sumplous.sumplous@gmail.com>
2025-12-05 19:24:33 +0100

    works for example

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -12,22 +12,47 @@ fn main() {
     let mut sum: u64 = 0;
     if let Ok(lines) = reader("../input/example.txt") {
         for line in lines.map_while(Result::ok) {
+            println!("Line -> {}", line);
             sum += joltage(line);
         }
     }
     else {
         println!("Damn file...");
     }
-    println!("Sum {}", sum);
+    println!("Total! {}", sum);
 }
 
 fn joltage(line: String) -> u64 {
-    let mut sum: u64 = 0;
+    let max: u64 = line.len().try_into().unwrap();
     let single_nums = line.split("");
+    let repeat = single_nums.clone();
     let mut first: u64 = 0;
     let mut second: u64 = 0;
+    let mut counter: u64 = 0;
+    let mut position: u64 = 0;
     for num in single_nums {
-        println!("{}", num);
+        let check: u64 = match num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        if first < check && counter < max - 1 {
+            first = check;
+            position = counter;
+        }
+        counter += 1;
     }
+    counter = 0;
+    for num in repeat {
+        let check: u64 = match num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        if counter > position && second < check {
+            second = check;
+        }
+        counter += 1;
+    }
+    let sum: u64 = (first * 10) + second;
+    println!("Sum -> {}", sum);
     sum
 }
