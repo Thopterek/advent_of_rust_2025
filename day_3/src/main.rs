8d997aec1e95b13e94fafaa2a5bbf812b9c80206
Thopterek <sumplous.sumplous@gmail.com>
2025-12-06 12:26:33 +0100

    day 3 finished, working with powers and usize

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use num_traits::Pow;
 
 fn reader<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -9,17 +10,59 @@ fn reader<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P:
 
 fn main() {
     println!("Day 3!");
-    let mut sum: u64 = 0;
+    let mut sum: usize = 0;
     if let Ok(lines) = reader("../input/puzzle.txt") {
         for line in lines.map_while(Result::ok) {
             println!("Line -> {}", line);
-            sum += joltage(line);
+            sum += joltage_part_two(line);
         }
     }
     else {
         println!("Damn file...");
     }
     println!("Total! {}", sum);
+}
+
+fn joltage_part_two(line: String) -> usize {
+    let max: usize = line.len().try_into().unwrap();
+    let mut all_twelve: [usize; 12] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mut position: usize = 0;
+    let mut i: usize = 0;
+    while i < 12 {
+        let single_nums = line.split("");
+        let mut counter: usize = 0;
+        for num in single_nums {
+            let check: usize = match num.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            if i != 0 {
+                if all_twelve[i] < check && counter < max - (11 - i) && counter > position {
+                    all_twelve[i] = check;
+                    position = counter;
+                }
+            }
+            else {
+                if all_twelve[i] < check && counter < max - (11 - i) {
+                    all_twelve[i] = check;
+                    position = counter;
+                }
+            }
+            counter += 1;
+        }
+        i += 1;
+    }
+    let mut sum: usize = 0;
+    i = 0;
+    let ten: usize = 10;
+    while i < 12 {
+        let to_power: usize = 11 - i;
+        let times: usize = Pow::pow(ten, to_power);
+        sum += all_twelve[i] * times;
+        i += 1;
+    }
+    println!("Sum of the damn thing -> {}", sum);
+    sum
 }
 
 fn joltage(line: String) -> u64 {
