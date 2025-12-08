74552c99c8f0235ea8f7badeddf66c2ad889010d
Nataniel Dziadzia <ndziadzi@2-E-8.42heilbronn.de>
2025-12-08 10:43:01 +0100

    starting day4 setup to read the input

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn reader<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = reader("../input/example.txt") {
+        for line in lines.map_while(Result::ok) {
+            println!("Line -> {}", line);
+        }
+    }
+    else {
+        println!("Run from src folder, should be fine");
+    }
 }
