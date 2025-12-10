304e51beab18351f4233e5aff234181c0968034d
Thopterek <sumplous.sumplous@gmail.com>
2025-12-10 01:08:13 +0100

    I think I have fixed the error and also made it easier with amount of things I am pushing into the stack

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -11,15 +11,30 @@ where P: AsRef<Path>, {
 fn main() {
     println!("FRESH WESH");
     let mut fresh: usize = 0;
-    if let Ok(lines) = reader("../input/example.txt") {
-        let mut ranges: Vec<u32>;
+    if let Ok(lines) = reader("../input/puzzle.txt") {
+        let mut ranges: Vec<usize> = vec![];
         let mut start_checking: bool = false;
         for line in lines.map_while(Result::ok) {
             println!("{}", line);
-            if start_checking == false {
-                let range = line.split("")
+            if start_checking == false && line.is_empty() == false {
+                let range = line.split_once("-");
+                let start: usize = range.unwrap().0.parse().expect("A NUMBER");
+                let end: usize = range.unwrap().1.parse().expect("A NUMBER");
+                ranges.push(start);
+                ranges.push(end);
             }
-            if line.is_empty() {
+            if start_checking == true {
+                let converted: usize = line.parse().expect("A NUMBER");
+                let mut index = 0;
+                while index < ranges.len() {
+                    if converted >= ranges[index] && converted <= ranges[index + 1] {
+                        fresh += 1;
+                        break;
+                    }
+                    index += 2;
+                }
+            }
+            if line.is_empty() == true {
                 start_checking = true;
             }
         }
