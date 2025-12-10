83161b4441c86bd93c35aa1f8b6dcbd1a5bb68ed
Thopterek <nataniel.dziadzia@gmail.com>
2025-12-10 14:29:30 +0100

    should be working just needs to so much time to run

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashSet;
 
 fn reader<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -11,7 +12,7 @@ where P: AsRef<Path>, {
 fn main() {
     println!("FRESH WESH");
     let mut fresh: usize = 0;
-    if let Ok(lines) = reader("../input/example.txt") {
+    if let Ok(lines) = reader("../input/puzzle.txt") {
         let mut ranges: Vec<usize> = vec![];
         let mut start_checking: bool = false;
         for line in lines.map_while(Result::ok) {
@@ -20,19 +21,39 @@ fn main() {
                 let range = line.split_once("-");
                 let start: usize = range.unwrap().0.parse().expect("A NUMBER");
                 let end: usize = range.unwrap().1.parse().expect("A NUMBER");
-                ranges.push(start);
-                ranges.push(end);
+                for i in start..end + 1 {
+                    ranges.push(i);
+                    println!("pushed {i}");
+                }
+                // ranges.push(start);
+                // ranges.push(end);
             }
             if start_checking == true {
+                // nevermind I don't think it will be needed
                 let mut index: usize = 0;
+                while index < ranges.len() {
+                    fresh += (ranges[index + 1] + 1) - ranges[index];
+                    println!("fresh now is {fresh}");
+                    index += 2;
+                }
+                break;
             }
             if line.is_empty() == true {
                 start_checking = true;
+                fresh += remove_dups(ranges);
+                break;
             }
         }
     }
     println!("Fresh ingridents -> {}", fresh);
 }
+
+fn remove_dups(data: Vec<usize>) -> usize {
+    let set: HashSet<_> = data.into_iter().collect();
+    let total: usize = set.len();
+    total
+}
+
 // part used in start_checking for part 1
 //            if start_checking == true {
 //                let converted: usize = line.parse().expect("A NUMBER");
