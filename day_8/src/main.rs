e6673ba8b48393e15dbbbf571902f9d08e63c6fd
salvador.dali.disciple@gmail.com <salvador.dali.disciple@1b5.cluster>
2025-12-12 14:00:46 +0100

    prep for later day_8

---
+++ day_8/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn reader<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    if let Ok(lines) = reader("../input/example.txt") {
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line);
+        }
+    }
+    else {
+        println!("WHERE IS THE FILE?!");
+    }
+}
